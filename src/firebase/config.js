dd5aecadd281fe07176bf844e25c4d503908ff98
AlejandroRibera <alejandroribera1987@gmail.com>
2024-02-28 14:33:40 -0300

    Actualizacion para la integracion de Base de Datos

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQIMNr3w4KpUvbRPMTydziBL-sqlfJ7Lw",
+  authDomain: "stockflow-adae7.firebaseapp.com",
+  projectId: "stockflow-adae7",
+  storageBucket: "stockflow-adae7.appspot.com",
+  messagingSenderId: "319208728487",
+  appId: "1:319208728487:web:acfba4ca3c044f32bcf882"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getFirestore(app);
+
